f5b87ebbd77fd3a950b5200c456b5cfe046dd057
aturubar@aligntech.com <aturubar@aligntech.com>
2017-05-24 13:56:37 +0300

    task2: add another constraint to fibonaci

--- task2/fibonaci.rb
+++ task2/fibonaci.rb
@@ -2,15 +2,13 @@ MAX_VALUE = 100 # last value must be less than this in fibonaci range
 fibonaci_array = [0]
 
 (1..100).each do |num|
+  break if num > MAX_VALUE
   if num < 3 
     fibonaci_array << 1 
   else
     next_value = fibonaci_array[-1] + fibonaci_array[-2]
-    if next_value > MAX_VALUE
-      break
-    else
-      fibonaci_array << next_value
-    end
+    break if next_value > MAX_VALUE
+    fibonaci_array << next_value
   end
 end
 
